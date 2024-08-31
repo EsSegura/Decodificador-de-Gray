077cbd6b42154c52fbcf6cf58779df45b3ec0dbf
Esteban SeGa <104318574+EsSegura@users.noreply.github.com>
2024-08-31 16:10:33 -0600

    Update module_tb.v

--- src/sim/module_tb.v
+++ src/sim/module_tb.v
@@ -28,32 +28,66 @@ module test;
     end
     
     initial begin
-        
         rst_i = 0;
         #30;
         rst_i = 1;
 
+        // Display para resultados
+        $display("Gray Code | Binary Code | Decimal");
+        $display("----------|-------------|--------");
+
+        // Probar valores Gray
         codigo_gray_i = 4'b0000; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b0001; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b0011; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b0010; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b0110; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b0111; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b0101; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b0100; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b1100; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b1101; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b1111; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b1110; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b1010; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b1011; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b1001; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
+        
         codigo_gray_i = 4'b1000; #100;
+        $display("%b   | %b   | %d", codigo_gray_i, codigo_bin_led_o, codigo_bin_led_o);
 
         #1000;
         $finish;
     end
-
 
     initial begin
         $dumpfile("module_deco_gray.vcd");
